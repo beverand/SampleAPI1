22523869a38cb70db76f5de847c1f44b217af1dc
beverand <beverly.andrews@gmail.com>
2023-02-09 23:26:01 -0800

    fetches data correctly

--- server.js
+++ server.js
@@ -7,15 +7,23 @@ const PORT = process.env.PORT || 3000
 const uri = process.env.DB_STRING;
 const client = new MongoClient(uri);
 
-app.get("/questions", async (req, res) => {
-    let questions = await client.connect()
-    return res.json(questions)
-})
-
 client.connect(err => {
     if(err){ console.error(err); return false;}
     // connection to mongo is successful, listen for requests
     app.listen(PORT, () => {
-        console.log("listening for requests");
+        console.log(`listening for requests on ${PORT}`);
     })
 });
+
+app.get("/questions", async (req, res) => {
+   let questions = await client.db("DailyDev")
+                               .collection("questions")
+                               .find({})
+                               .project({ question:1, qtype:1, _id:0 }).toArray();
+
+   if(questions){
+     res.json(questions);
+   } else {
+     res.sendStatus(404);
+   }  
+})
