a791a639c0b38d8d0b0bd7c20bcb19a3531d2c6b
beverand <beverly.andrews@gmail.com>
2023-02-10 20:28:42 -0800

    mod add additional endpoints

--- server.js
+++ server.js
@@ -16,14 +16,76 @@ client.connect(err => {
 });
 
 app.get("/questions", async (req, res) => {
+   //all questions
    let questions = await client.db("DailyDev")
                                .collection("questions")
                                .find({})
                                .project({ question:1, qtype:1, _id:0 }).toArray();
-
    if(questions){
      res.json(questions);
    } else {
      res.sendStatus(404);
    }  
+})
+
+app.get("/qtype/:qtype", async (req, res) => {
+  //get all questions in one type
+  const { qtype } = req.params;
+  let questions = client.db("DailyDev")
+                        .collection("questions")
+                        .find({qtype:qtype}).project({ question:1, qtype:1, _id:0 }).toArray();
+
+  if(questions){
+    res.json(questions);
+  } else {
+    res.sendStatus(404);
+  }
+})
+
+app.get("/qsearch/:search", async (req, res) => {
+  //get all questions that contain search criteria
+  const { search } = req.params;
+  let questions = client.db("DailyDev")
+                        .collection("questions")
+                        .find({question:new RegExp('.*' + search + '.*')}).project({ question:1, qtype:1, _id:0 }).toArray();
+
+  if(questions){
+    res.json(questions);
+  } else {
+    res.sendStatus(404);
+  }
+})
+
+app.get("/questions/:qid", async (req, res) => {
+  //get single question by id 
+  const {  qid } = req.params;
+  let questions = client.db("DailyDev")
+                        .collection("questions")
+                        .aggregate([
+                           {
+                              '$match': {
+                                 'questions._id': new ObjectId(qid)
+                               }
+                           }
+                         ])
+                        .toArray();
+ if(questions){
+    res.json(questions);
+  } else {
+    res.sendStatus(404);
+  }
+})
+
+app.get("/qsamp/:qcount", async (req, res) => {
+  //get single question by id 
+  const {  qcount } = req.params;
+  let questions = client.db("DailyDev")
+                        .collection("questions")
+                        .aggregate([ { $sample: { size: Number(qcount)} } ])
+                        .toArray();
+ if(questions){
+    res.json(questions);
+  } else {
+    res.sendStatus(404);
+  }
 })
